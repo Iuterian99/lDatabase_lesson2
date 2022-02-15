ae212a054fef19483885567014e48256c7955fbb
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-15 22:33:40 +0500

    created restaurants, branchs and menu tables

--- src/model/model.sql
+++ src/model/model.sql
@@ -1,10 +1,28 @@
 -- Managing Tables
 
-CREATE TABLE courses (
+CREATE TABLE restaurants (
+restaurant_id serial PRIMARY KEY,
+restaurant_name varchar(32) not null,
+working_hours int not null
+);
+
+CREATE TABLE branchs (
+branch_id serial PRIMARY KEY,
+branch_location varchar(32) not null,
+working_hours int not null,
+location_info text,
+branch_created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+branch int REFERENCES restaurants(restaurant_id)
+);
+
+CREATE TABLE branchMenus (
 course_id serial PRIMARY KEY,
-course_name varchar(32) not null,
-course_price decimal(10,2) not null,
-curse_duration int not null,
-course_desc text,
-course_created_at timestamptz DEFAULT CURRENT_TIMESTAMP
-)
+meal_name varchar(32) not null,
+meal_desc text,
+menu int REFERENCES branchs(branch_id)
+);
+
+INSERT INTO restaurants(
+  restaurant_name,
+  working_hours
+) VALUES ('Rayhon', 9:00 to 23:00);
