2f61fa40f017854c79904b397ce5b98c426d711e
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-02-15 23:41:49 +0500

    created branches

--- src/model/model.sql
+++ src/model/model.sql
@@ -3,13 +3,11 @@
 CREATE TABLE restaurants (
 restaurant_id serial PRIMARY KEY,
 restaurant_name varchar(32) not null,
-working_hours int not null
 );
 
 CREATE TABLE branchs (
 branch_id serial PRIMARY KEY,
 branch_location varchar(32) not null,
-working_hours int not null,
 location_info text,
 branch_created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
 branch int REFERENCES restaurants(restaurant_id)
@@ -22,7 +20,32 @@ meal_desc text,
 menu int REFERENCES branchs(branch_id)
 );
 
-INSERT INTO restaurants(
-  restaurant_name,
-  working_hours
-) VALUES ('Rayhon', 9:00 to 23:00);
+ALTER TABLE branchs DROP COLUMN working_hours;
+SELECT * FROM restaurants JOIN branchs ON restaurant_id = branch;
+
+INSERT INTO restaurants( restaurant_name) VALUES ('Rayhon');
+INSERT INTO restaurants( restaurant_name) VALUES ('Soy');
+INSERT INTO restaurants( restaurant_name) VALUES ('Komolon');
+INSERT INTO restaurants( restaurant_name) VALUES ('Beshqozon');
+
+
+INSERT INTO branchs( branch_location, location_info, branch) 
+VALUES ('Shayxontoxur',
+'Samarqand darvoza yonida', 
+1
+);
+INSERT INTO branchs( branch_location, location_info, branch) 
+VALUES ('Yunusobot',
+'Megaplanet yonida', 
+1
+);
+INSERT INTO branchs( branch_location, location_info, branch) 
+VALUES ('Chilonzor',
+'Integro yonida', 
+1
+);
+INSERT INTO branchs( branch_location, location_info, branch) 
+VALUES ('Yashnabod',
+'Park yonida', 
+1
+);
